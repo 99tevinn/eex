612339f3d86f86670a0de2ffc089576579e2e432
99tevinn <tevinodiwuor@gmail.com>
2025-03-04 15:27:03 +0300

    revision

--- template.exs
+++ template.exs
@@ -1,32 +1,56 @@
 defmodule MyEngine do
   @behaviour EEx.Engine
 
-  def init(_opts), do: %{}
+  def init(_opts), do: []
 
-  def handle_begin(state) do
-    IO.inspect({"Begin", state})
-    state
+
+  def handle_begin(_state) do
+    IO.inspect("Begin")
+    [quote do: IO.write("BEGIN: ")]
   end
 
   def handle_body(state) do
     IO.inspect({"Body", state})
-    state
+
+    quote do
+      unquote_splicing(state)
+    end
   end
 
   def handle_text(state, _meta, text) do
     IO.inspect({"Text", text})
-    state
+
+    state ++ [quote do: IO.write(unquote(text))]
+  end
+
+  def handle_expr(state, _marker, {:if, _, _} = expr) do
+    IO.inspect({"Expr - Control Structure", expr})
+
+    state ++ [expr]
   end
 
   def handle_expr(state, _marker, expr) do
     IO.inspect({"Expr", expr})
-    state
+
+    state ++ [quote do: IO.write(to_string(unquote(expr)))]
   end
 
-  def handle_end(quoted) do
-    IO.inspect("Finished!")
-    quoted
+  # Handle the end of the template
+  def handle_end(state) do
+    IO.inspect("End")
+
+    state ++ [quote do: IO.write(" :END")]
   end
 end
 
-EEx.compile_string("Hello <%= name %>", engine: MyEngine)
+# Sample Template
+template = """
+<%= if true do %>
+  Hello, <%= name %>! Today is <%= day %>.
+<% end %>
+"""
+
+# Evaluate the template using MyEngine
+result = EEx.eval_string(template, [name: "Alice", day: "Tuesday"], engine: MyEngine)
+
+IO.puts("\nFinal Output: #{result}")
