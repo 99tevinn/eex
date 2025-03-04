d7eb8079b96c9c7ad60d36871eee07c049fac31a
99tevinn <tevinodiwuor@gmail.com>
2025-03-04 11:12:50 +0300

    continuer

--- template.exs
+++ template.exs
@@ -1,11 +1,32 @@
-defmodule Tev do
-  @ast EEx.compile_file("tev.eex")
+defmodule MyEngine do
+  @behaviour EEx.Engine
 
-  def render(user) do
-    {result, _} = Code.eval_quoted(@ast, user: user)
-    result
+  def init(_opts), do: %{velocity: 1}
+
+  def handle_begin(state) do
+    IO.inspect({"Begin", state})
+    state
+  end
+
+  def handle_body(state) do
+    IO.inspect({"Body", state})
+    state
+  end
+
+  def handle_text(state, _meta, text) do
+    IO.inspect({"Text", text})
+    state
+  end
+
+  def handle_expr(state, _marker, expr) do
+    IO.inspect({"Expr", expr})
+    state
+  end
+
+  def handle_end(quoted) do
+    IO.inspect("Finished!")
+    quoted
   end
 end
 
-user = %{name: "Tevin", age: 17}
-IO.puts(Tev.render(user))
+EEx.compile_string("Hello <%= name %>", engine: MyEngine)
